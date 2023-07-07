f3a1b77177f1973430fafc52fcfa395613560f13
Lay7415 <ieye7531@gmail.com>
2023-07-07 13:16:08 +0300

    new: create postgresql config file

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,9 @@ const userController = require('../controllers/user.controller')
 
 const usersRouter = Router()
 
-usersRouter.post('/registration', userController.registration)
-usersRouter.post('/login', userController.login)
-usersRouter.get('/refresh', userController.refresh)
-usersRouter.post('/logout', userController.logout)
+usersRouter.post('/user/registration', userController.registration)
+usersRouter.post('/user/login', userController.login)
+usersRouter.get('/user/refresh', userController.refresh)
+usersRouter.post('/user/logout', userController.logout)
 
 module.exports = usersRouter
